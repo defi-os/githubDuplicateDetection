e69a01155fa0d557b3a1e171d7b9a39412f4d02b
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-05-22 18:51:51 +0530

    added helper functions for lsi

--- parsers/parse_description.py
+++ parsers/parse_description.py
@@ -5,6 +5,15 @@ import nltk
 from collections import Counter
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+import os.path
+from gensim import corpora
+from gensim.models import LsiModel
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from gensim.models.coherencemodel import CoherenceModel
+import matplotlib.pyplot as plt
+
 stop_words = set(stopwords.words('english'))
 
 def split_text(text:str) -> list:
@@ -21,4 +30,76 @@ def generate_tf_idf(text: str) -> Counter:
     for word in formatted_text_dict:
         formatted_text_dict[word] = formatted_text_dict[word]/count
     return formatted_text_dict
+
+def preprocess_data(doc_set):
+    """
+    Input  : docuemnt list
+    Purpose: preprocess text (tokenize, removing stopwords, and stemming)
+    Output : preprocessed text
+    """
+    # initialize regex tokenizer
+    tokenizer = RegexpTokenizer(r'\w+')
+    # Create p_stemmer of class PorterStemmer
+    p_stemmer = PorterStemmer()
+    # list for tokenized documents in loop
+    texts = []
+    #removes stop words from document list
+    doc_set = remove_stopwords(doc_set)
+    # loop through document list
+    for i in doc_set:
+        # clean and tokenize document string
+        raw = i.lower()
+        tokens = tokenizer.tokenize(raw)
+        # stem tokens
+        stemmed_tokens = [p_stemmer.stem(i) for i in tokens]
+        # add tokens to list
+        texts.append(stemmed_tokens)
+    return texts
+
+def prepare_corpus(doc_clean):
+    """
+    Input  : clean document
+    Purpose: create term dictionary of our courpus and Converting list of documents (corpus) into Document Term Matrix
+    Output : term dictionary and Document Term Matrix
+    """
+    # Creating the term dictionary of our courpus, where every unique term is assigned an index. dictionary = corpora.Dictionary(doc_clean)
+    dictionary = corpora.Dictionary(doc_clean)
+    # Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
+    doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
+    # generate LDA model
+    return dictionary,doc_term_matrix
+
+def create_gensim_lsa_model(doc_clean,number_of_topics,words):
+    """
+    Input  : clean document, number of topics and number of words associated with each topic
+    Purpose: create LSA model using gensim
+    Output : return LSA model
+    """
+    dictionary,doc_term_matrix=prepare_corpus(doc_clean)
+    # generate LSA model
+    lsamodel = LsiModel(doc_term_matrix, num_topics=number_of_topics, id2word = dictionary)  # train model
+    print(lsamodel.print_topics(num_topics=number_of_topics, num_words=words))
+    return lsamodel
+
+def compute_coherence_values(dictionary, doc_term_matrix, doc_clean, stop, start=2, step=3):
+    """
+    Input   : dictionary : Gensim dictionary
+              corpus : Gensim corpus
+              texts : List of input texts
+              stop : Max num of topics
+    purpose : Compute c_v coherence for various number of topics
+    Output  : model_list : List of LSA topic models
+              coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, stop, step):
+        # generate LSA model
+        model = LsiModel(doc_term_matrix, num_topics=num_topics, id2word = dictionary)  # train model
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=doc_clean, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+    return model_list, coherence_values
+
+
 
