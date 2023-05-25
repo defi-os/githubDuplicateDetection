00f9d9a21bb1845f32c80e6fa4093a5217585ebb
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-05-25 20:54:28 +0530

    added write to md helper function

--- model/Dataset/scrap_data/scrap_pr.py
+++ model/Dataset/scrap_data/scrap_pr.py
@@ -4,5 +4,7 @@ from utils import select_unique_pairs, write_to_md
 
 for repo in major_repos:
     recent_pull_requests = get_recent_merged_pull_requests(repo)
-    unique_pull_request_pairs = select_unique_pairs(recent_pull_requests,recent_pull_requests//4,recent_pull_requests*4)
-    write_to_md(unique_pull_request_pairs)
+    unique_pull_request_pairs = select_unique_pairs(
+        recent_pull_requests, recent_pull_requests // 4, recent_pull_requests * 4
+    )
+    write_to_md(repo, unique_pull_request_pairs)
--- model/Dataset/scrap_data/utils.py
+++ model/Dataset/scrap_data/utils.py
@@ -1,12 +1,29 @@
 import random
-def select_unique_pairs(lst,number_pairs,max_iterations):
+
+
+def select_unique_pairs(lst: list, number_pairs: int, max_iterations: int) -> list:
     pairs = set()
     iterations = 0
-    
+
     while len(pairs) < number_pairs and iterations < max_iterations:
         pair = random.sample(lst, 2)
         if pair[0] != pair[1] and pair not in list(pairs):
             pairs.add(tuple(pair))
-            
+
         iterations += 1
     return list(pairs)
+
+
+def write_to_md(repo: str, pull_requests_list: list):
+    f = open("scraped_data.md", "a")
+    for pull_requests in pull_requests_list:
+        pull_request_nos = [pr.split("pull/")[1] for pr in pull_requests]
+        append_data = "| {} | [{}]({}), [{}]({}) |\n".format(
+            repo,
+            pull_request_nos[0],
+            pull_requests[0],
+            pull_request_nos[1],
+            pull_requests[1],
+        )
+        f.write(append_data)
+    f.close()
