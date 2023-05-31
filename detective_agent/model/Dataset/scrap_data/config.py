961bde359e65d185f2de1878c1c0020b144a419c
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-06-01 01:25:14 +0530

    added some more repos to config.py

--- detective_agent/model/Dataset/scrap_data/config.py
+++ detective_agent/model/Dataset/scrap_data/config.py
@@ -1,1 +1,12 @@
-major_repos = ["Zulip/zulip"]
+major_repos = [
+    "Zulip/zulip",
+    "blockscout/blockscout",
+    "ggerganov/whisper.cpp",
+    "Significant-Gravitas/Auto-GPT",
+    "golang/go",
+    "googleapis/googleapis",
+    "metabase/metabase",
+    "amazon-sagemaker-examples",
+    "NomicFoundation/hardhat",
+    "safe-global/safe-wallet-web"
+]
